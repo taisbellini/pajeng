1325bdeb5f35eb7e731a99c1443658bb6a451d24
Tais Bellini <taisbellini@MacBook-Pro-de-James.local>
2015-12-22 10:54:19 -0200

    MySQL connection

--- workspace/JavaCCPajeParser/src/br/ufrgs/inf/tlbellini/lib/DBClass.java
+++ workspace/JavaCCPajeParser/src/br/ufrgs/inf/tlbellini/lib/DBClass.java
@@ -2,23 +2,54 @@ package br.ufrgs.inf.tlbellini.lib;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DBClass {
-	
-	public void connectDB(){
-		Connection c = null;
-	      try {
-	         Class.forName("org.postgresql.Driver");
-	         c = DriverManager
-	            .getConnection("jdbc:postgresql://localhost:5432/postgres",
-	            "postgres", "postgres");
-	      } catch (Exception e) {
-	         e.printStackTrace();
-	         System.err.println(e.getClass().getName()+": "+e.getMessage());
-	         System.exit(0);
-	      }
-	      System.out.println("Opened database successfully");
-	   }	      
+
+	public String status = "Não conectou...";
+	public DBClass() {}
+
+	public java.sql.Connection connectDB() { 
+		Connection connection = null;
+		
+		try { 
+			String driverName = "com.mysql.jdbc.Driver"; 
+			Class.forName(driverName); 
+			String serverName = "localhost"; 
+			String mydatabase = "paje"; 
+			String useSSL ="?verifyServerCertificate=false" + "&useSSL=false";
+			String url = "jdbc:mysql://" + serverName + "/" + mydatabase + useSSL; 
+			String username = "root";  
+			String password = "root"; 
+			connection = DriverManager.getConnection(url, username, password); 
+			if (connection != null) { 
+				status = ("STATUS--->Conectado com sucesso!"); 
+			} else { 
+				status = ("STATUS--->Não foi possivel realizar conexão"); 
+			} return connection; } 
+
+		catch (ClassNotFoundException e) { 
+			System.out.println("O driver expecificado nao foi encontrado."); 
+			return null; 
+		} catch (SQLException e) { 
+			System.out.println("Nao foi possivel conectar ao Banco de Dados."); 
+			return null; 
+		} 
+	} 
+	public String statusConection() { 
+		return status; 
+	} 
+	public boolean FecharConexao() { 
+		try { 
+			this.connectDB().close(); 
+			return true; } 
+		catch (SQLException e) 
+		{ 
+			return false; 
+		} 
+	} 
+
 }
+
 
 
