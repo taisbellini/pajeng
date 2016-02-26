aa73adf7cb7b217e1e3b2f6c4f4ecab58080d026
Lucas Schnorr <schnorr@inf.ufrgs.br>
2016-02-26 17:45:33 -0300

    fix to avoid CLASSPATH problems, again

--- workspace/compile.sh
+++ workspace/compile.sh
@@ -9,5 +9,6 @@ javac br/ufrgs/inf/tlbellini/lib/*.java
 javac br/ufrgs/inf/tlbellini/*.java
 popd
 export CLASSPATH=`pwd`/JavaCCPajeParser/src/
+export CLASSPATH=$CLASSPATH:/home/schnorr/svn/tais/tais-pajeng/workspace/include/mysql-connector-java-5.1.38-bin.jar:.
 echo "Execute the program by typing:"
 echo "$ java br.ufrgs.inf.tlbellini.PajeGrammar <path to input Paje tracefile>"
